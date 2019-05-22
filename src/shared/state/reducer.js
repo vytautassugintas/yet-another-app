d34b2c0743d4f9541afa72e1a08088061c515d49
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2019-05-22 19:31:38 +0300

    refactor: moved fn to its own file

--- src/shared/state/reducer.js
+++ src/shared/state/reducer.js
@@ -7,6 +7,7 @@ import {
   increaseCupsCount
 } from "./updators";
 import { actions } from "./actions";
+import { createReducer } from "../helpers/createReducer";
 
 export const initialState = {
   view: "LOG",
@@ -18,16 +19,6 @@ export const initialState = {
   selectedIngredients: [],
   meals: []
 };
-
-function createReducer(initialState, handlers) {
-  return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action);
-    } else {
-      return state;
-    }
-  };
-}
 
 export const reducer = createReducer(initialState, {
   [actions.CHANGE_VIEW]: changeView,
